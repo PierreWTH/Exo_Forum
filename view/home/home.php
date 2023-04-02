bffdb49a94011f55797c6d1f9399b94082a4c0a2
PierreWTH <wietrichpierre@gmail.com>
2023-04-02 21:11:42 +0200

    page home css

--- view/home/home.php
+++ view/home/home.php
@@ -1,9 +1,15 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
-
-<p>Bienvenue sur ce beau forum.</p>
-
-<p>
-    <a href="/security/login.html">Se connecter</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="/security/register.html">S'inscrire</a>
-</p>
+<div id = "forum-home">
+    <div class = "titre-page">
+    <h1>BIENVENUE ! </h1>
+    </div>
+    <div class = "contenu-home">
+    <p>Bienvenue sur ce beau forum. Vous pouvez vous connecter si vous êtes déja membre, vous inscrire si vous ne l'êtes pas encore, et commencer a poster de messages et créer des topics. Attention, restez respectueux, sinon vous serez banni selon la gravité de ce que vous avez fait. Je peux aussi bannir sans raison car je suis l'admin après tout. </p>
+    </div>
+    <div class = "home-login-links">
+    <p>
+        <a href="index.php?ctrl=security&action=loginView"><i class="fa-solid fa-user"></i> Se connecter</a>
+        <span>&nbsp;-&nbsp;</span>
+        <a href="index.php?ctrl=security&action=index"><i class="fa-regular fa-address-card"></i> S'inscrire</a>
+    </p>
+    </div>
+</div>
